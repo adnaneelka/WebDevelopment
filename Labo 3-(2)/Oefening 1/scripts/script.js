d826bf4298c7a7d8a6392f64ee5b0bc7c278d03f
adnaneelka <adnane.elkaddouri@student.vives.be>
2024-03-02 00:32:33 +0100

    web

---
+++ Labo 3-(2)/Oefening 1/scripts/script.js
@@ -0,0 +1,7 @@
+const setup = () => {
+    let belangrijk = document.getElementsByClassName("belangrijk");
+    for (let i = 0; i < belangrijk.length; i++) {
+        belangrijk[i].classList.add("opvallend");
+    }};
+
+window.addEventListener("load", setup);
